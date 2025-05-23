ab7c818714985e42a0b23594ece5e749528218c1
RamMurari99 <rammurari3528@gmail.com>
2025-05-23 10:30:00 +0530

    Update weather UI styles and fix layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MoviesService } from './movies.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
- searchText = '';
+export class AppComponent implements OnInit {
+  searchText = '';
   movies: any[] = [];
   error = '';
 
   constructor(private moviesService: MoviesService) {}
 
+  ngOnInit(): void {
+    this.loadDefaultMovies();
+  }
+
+  loadDefaultMovies(): void {
+    this.moviesService.searchMovies('Avengers').subscribe({
+      next: (response) => {
+        if (response.Response === 'True') {
+          this.movies = response.Search;
+          this.error = '';
+        } else {
+          this.error = response.Error;
+        }
+      },
+      error: () => {
+        this.error = 'Failed to load default movies.';
+      }
+    });
+  }
+
   search(): void {
+    if (!this.searchText.trim()) return;
+
     this.moviesService.searchMovies(this.searchText).subscribe({
       next: (response) => {
         if (response.Response === 'True') {
